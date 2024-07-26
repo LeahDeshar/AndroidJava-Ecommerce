99a5d987beea4fb012d73b216ef7386d8060ef98
Leah Deshar <leah.desar@gmail.com>
2024-07-26 16:19:54 +0545

    Ecomm: activity UI

--- app/src/main/java/com/example/ecomm/BaseActivity.java
+++ app/src/main/java/com/example/ecomm/BaseActivity.java
@@ -8,14 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends BaseActivity {
-    ActivityIntroBinding binding;
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
     }
 }
