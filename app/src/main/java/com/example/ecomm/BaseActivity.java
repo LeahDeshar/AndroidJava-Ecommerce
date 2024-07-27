6c00ee64076b80b4d6e970af21019521027a71a3
Leah Deshar <leah.desar@gmail.com>
2024-07-27 10:15:29 +0545

    Dashboard

--- app/src/main/java/com/example/ecomm/BaseActivity.java
+++ app/src/main/java/com/example/ecomm/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.example.ecomm;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -29,10 +30,20 @@ public class BaseActivity extends AppCompatActivity {
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-        WindowInsetsController insetsController = window.getInsetsController();
-        if (insetsController != null) {
-            insetsController.hide(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-            insetsController.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        WindowInsetsController insetsController = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            insetsController = getWindow().getInsetsController();
         }
+        if (insetsController != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                insetsController.hide(WindowInsetsCompat.Type.statusBars());
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                insetsController.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+        }
+
     }
 }
