751b70ac4626cd9b779b19a815b0463d11ccd6f2
Leah Deshar <leah.desar@gmail.com>
2024-07-27 09:59:07 +0545

    Dashboard

--- app/src/main/java/com/example/ecomm/BaseActivity.java
+++ app/src/main/java/com/example/ecomm/BaseActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecomm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
@@ -18,5 +21,18 @@ public class BaseActivity extends AppCompatActivity {
 //        full screen experience settings
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        WindowInsetsController insetsController = window.getInsetsController();
+        if (insetsController != null) {
+            insetsController.hide(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            insetsController.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
     }
 }
