0596d190d7493d2820f305a14937a8abbb19f199
Leah Deshar <leah.desar@gmail.com>
2024-08-12 19:16:00 +0545

    changes

--- app/src/main/java/com/example/ecomm/Activity/BaseActivity.java
+++ app/src/main/java/com/example/ecomm/Activity/BaseActivity.java
@@ -22,26 +22,26 @@ public class BaseActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
 
-//        Window window = getWindow();
-//        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-//                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-//
-//        WindowInsetsController insetsController = null;
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            insetsController = getWindow().getInsetsController();
-//        }
-//        if (insetsController != null) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//                insetsController.hide(WindowInsetsCompat.Type.statusBars());
-//            }
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//                insetsController.setSystemBarsBehavior(
-//                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-//                );
-//            }
-//        }
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        WindowInsetsController insetsController = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            insetsController = getWindow().getInsetsController();
+        }
+        if (insetsController != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                insetsController.hide(WindowInsetsCompat.Type.statusBars());
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                insetsController.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+        }
 
     }
 }
